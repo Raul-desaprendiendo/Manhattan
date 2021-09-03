98726531351f8ddb8c86a0bb7c1a1099e40ae638
altitoo <59035217+altitoo@users.noreply.github.com>
2021-09-03 12:38:28 +0200

    Manhattan

---
+++ Manhattan/DistanceService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometriaDelTaxista
+{
+    
+    public class DistanceService : IDistanceMeasurement
+    {
+        public int Calculate(Point p1 , Point p2)
+        {
+          
+            return p1.GetDistanceRow(p2) + p1.GetDistanceColumn(p2);
+        }
+        
+    }
+}
--- Manhattan/Program.cs
+++ Manhattan/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 
-namespace Manhattan
+namespace GeometriaDelTaxista
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DistanceService service = new DistanceService();
+            
+            Console.WriteLine(service.Calculate(new Point(1,1), new Point(1,1)));
+            Console.WriteLine(service.Calculate(new Point(5, 4), new Point(3, 2)));
+            Console.WriteLine(service.Calculate(new Point(1, 1), new Point(0, 3)));
+
+
         }
     }
 }
