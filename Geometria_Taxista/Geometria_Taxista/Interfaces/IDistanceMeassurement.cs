f91a066d7b6f3a5a5649ef2bcfd7ee90aa44e405
Mnavarro1997 <89913076+Mnavarro1997@users.noreply.github.com>
2021-09-04 09:49:55 +0200

    Add files via upload

---
+++ Geometria_Taxista/Geometria_Taxista/Interfaces/IDistanceMeassurement.cs
@@ -0,0 +1,19 @@
+﻿
+using Geometria_Taxista.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometria_Taxista.Interfaces
+{
+    public interface IDistanceMeassurement
+    {
+        /// <summary>
+        /// action of calculate.
+        /// </summary>
+        /// <param name="p1"></param>
+        /// <param name="p2"></param>
+        /// <returns></returns>
+        public int CalculateDistance(Point p1, Point p2);
+    }
+}
---
+++ Geometria_Taxista/Geometria_Taxista/Models/DistanceService.cs
@@ -0,0 +1,24 @@
+﻿using Geometria_Taxista.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometria_Taxista.Models
+{
+    /// <summary>
+    /// extends of the interface to get the points.
+    /// </summary>
+    public class DistanceService : IDistanceMeassurement
+    {
+        /// <summary>
+        /// Here I calculate the point.
+        /// </summary>
+        /// <param name="p1"></param>
+        /// <param name="p2"></param>
+        /// <returns>we get p1 of row p2, and the same one with columns</returns>
+        public int CalculateDistance(Point p1, Point p2)
+        {
+            return p1.GetRow(p2) + p1.GetColumn(p2);
+        }
+    }
+}
