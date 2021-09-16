30112a63db160afc729369951526f404ab2d8714
JavierSG <jsanudo@gteam.es>
2021-09-16 13:43:29 +0200

    Primer commit

--- Manhattan/Program.cs
+++ Manhattan/Program.cs
@@ -2,11 +2,11 @@
 
 namespace Manhattan
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!s");
         }
     }
 }
