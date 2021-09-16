d911947834f05f48d5f58d03da782187048f17ff
Selene <selene.ajtd@gmail.com>
2021-09-16 19:05:14 +0200

    Folder models.

---
+++ Manhattan/Models/Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Manhattan
+{
+    internal class Point
+    {
+        private int _x;
+        private int _y;
+
+        public Point(int columna, int row)
+        {
+            _x = columna;
+            _y = row;
+        }
+
+        private int GetX()
+        {
+            return _x;
+        }
+
+        private int GetY()
+        {
+            return _y;
+        }
+    }
+}
