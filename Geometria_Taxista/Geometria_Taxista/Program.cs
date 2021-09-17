6053a483ea44657d34ff8c8db1835648b0da22c1
MNavarro1997 <mnavarro@gteam.es>
2021-09-17 13:26:58 +0200

    cambios y creacion de Service

--- Geometria_Taxista/Geometria_Taxista/Program.cs
+++ Geometria_Taxista/Geometria_Taxista/Program.cs
@@ -1,20 +1,18 @@
-﻿using Geometria_Taxista.Models;
-using System;
+﻿using System;
 
 namespace Geometria_Taxista
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            // I create the new parameter "d", to pass all the info.
-            DistanceService d = new DistanceService();
-            
-            // each point is throw to new points, and with the method of calculate create the result.
-            Console.WriteLine(d.CalculateDistance(new Point(1, 1), new Point(1, 1)));
-            Console.WriteLine(d.CalculateDistance(new Point(5, 4), new Point(3, 2)));
-            Console.WriteLine(d.CalculateDistance(new Point(1, 1), new Point(0, 3)));
+            // I create the new parameter "d", to pass all the info
+            //DistanceService d = new DistanceService();
 
+            //// each point is throw to new points, and with the method of calculate create the result.
+            //Console.WriteLine(d.CalculateDistance(new Point(1, 1), new Point(1, 1)));
+            //Console.WriteLine(d.CalculateDistance(new Point(5, 4), new Point(3, 2)));
+            //Console.WriteLine(d.CalculateDistance(new Point(1, 1), new Point(0, 3)));
         }
     }
 }
